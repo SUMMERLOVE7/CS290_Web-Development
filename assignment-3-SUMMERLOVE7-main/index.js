3f74ba5344c44f93ca9cfd1a089cce11154e3a3f
MJ.CH <81309465+SUMMERLOVE7@users.noreply.github.com>
2021-11-07 11:17:53 -0800

    Update modal features

--- assignment-3-SUMMERLOVE7-main/index.js
+++ assignment-3-SUMMERLOVE7-main/index.js
@@ -2,8 +2,8 @@
  * Write your client-side JS code in this file.  Don't forget to include your
  * name and @oregonstate.edu email address below.
  *
- * Name: 
- * Email: 
+ * Name: Min Ji Chang
+ * Email: changmin@oregonstate.edu
  */
 
 var showModal = document.getElementById('sell-something-button')
@@ -15,11 +15,20 @@ var photo = document.getElementById('post-photo-input')
 var price = document.getElementById('post-price-input')
 var city = document.getElementById('post-city-input')
 var condition = document.getElementsByName('post-condition')
+var updateButton = document.getElementById('filter-update-button')
+var filterTxt = document.getElementById('filter-text')
+var title = document.getElementsByClassName('post-title')
+var post = document.getElementsByClassName('post')
 
 function openModal(){
     document.getElementById('modal-backdrop').style.display = "block"
     document.getElementById('sell-something-modal').style.display = "block"
-    document.getElementById('sell-something-modal').classList.add("show") 
+    document.getElementById('sell-something-modal').classList.add("show")
+    
+    des.value = ''
+    photo.value = ''
+    price.value = ''
+    city.value = ''
 }
 
 function closeModal(){
@@ -50,52 +59,40 @@ removeModal_2.onclick = function(event){
     closeModal()
 }
 
-
-var newClass = document.createAttribute("class")
-
 function registerPost(){
-       /*
-    const newDes = des.value
-    const newPhoto = photo.value
-    const newPrice = price.value
-    const newCity = city.value
-    
 
     var newDiv = document.createElement("div")
-    var subject = document.querySelector('.filter-input')
-    var newinfo = document.createTextNode(subject.value)
-
-    newItem.appendChild(newinfo)
-    var itemList = document.querySelector('.filter-container')
-    itemList.appendChild(newItem)
-
-    */
-    var newDiv = document.createElement("div")
-    //var newPost = document.createAttribute("post")
-    var newDataPrice = document.createAttribute("data-price")
-    var newDataCity = document.createAttribute("data-city")
-    var newDataCondition = document.createAttribute("data-condition")
-
-    var newPostContents = document.createAttribute("post-contents")
+    var newClass = document.createAttribute("class")
 
     newClass.value = "post"
-    newDataPrice = price.value
-    newDataCity = city.value
-    newDataCondition = '미완'
-
     newDiv.setAttributeNode(newClass)
-    newDiv.setAttributeNode(newDataPrice)
-    newDiv.setAttributeNode(newDataCity)
-    newDiv.setAttributeNode(newDataCondition)
+
+    for(var i = 0; i< condition.length; i++){
+        if(condition[i].checked == true){
+            var newCondition = document.createTextNode(condition[i].value)
+        }
+    }
+
+    var dataPrice = document.createAttribute('data-price')
+    var dataCity = document.createAttribute('data-city')
+    var dataCondition = document.createAttribute('data-condition')
+    dataPrice.value = price.value
+    dataCity.value = city.value
+    dataCondition.value = newCondition.value
+    newDiv.setAttributeNode(dataPrice)
+    newDiv.setAttributeNode(dataCity)
+    newDiv.setAttributeNode(dataCondition)
 
     var newDiv2  = document.createElement("div")
-    newClass.value = "post-contents"
-    newDiv2.setAttributeNode(newClass)
-    newDiv.appendChildNode(newDiv2)
+    var newClass2 = document.createAttribute("class")
+    newClass2.value = "post-contents"
+    newDiv2.setAttributeNode(newClass2)
+    newDiv.appendChild(newDiv2)
 
     var newDiv3 = document.createElement("div")
-    newClass.value = "post-image-container"
-    newDiv3.setAttributeNode(newClass)
+    var newClass3 = document.createAttribute("class")
+    newClass3.value = "post-image-container"
+    newDiv3.setAttributeNode(newClass3)
     newDiv2.appendChild(newDiv3)
 
     var newPhoto = document.createElement("img")
@@ -109,25 +106,82 @@ function registerPost(){
     newDiv3.appendChild(newPhoto)
 
     var newDiv4 = document.createElement("div")
+    var newClass4 = document.createAttribute("class")
     var newA = document.createElement("a")
     var newHref = document.createAttribute("href")
+    var newClass5 = document.createAttribute("class")
     newHref.value = "#"
-    newClass.value = "post-title"
-    newA.setAttributeNode(newClass)
-    newA.classList.add('post-price')
-    newA.classList.add('post-city')
+    newClass5.value = "post-title"
+    newA.setAttributeNode(newHref)
+    newA.setAttributeNode(newClass5)
+
+    var newSpan = document.createElement("span")
+    var newClass6 = document.createAttribute("class")
+    newClass6.value = 'post-price'
+    newSpan.setAttributeNode(newClass6)
+
+    var newSpan2 = document.createElement("span")
+    var newClass7 = document.createAttribute("class")
+    newClass7.value = "post-city"
+    newSpan2.setAttributeNode(newClass7)
+
+    newClass4.value = "post-info-container"
+    newDiv4.setAttributeNode(newClass4)
     newDiv4.appendChild(newA)
+    newA.append(document.createTextNode(des.value))
+    newDiv4.appendChild(newSpan)
+    newSpan.append('$')
+    newSpan.append(document.createTextNode(price.value))
+    newDiv4.appendChild(newSpan2)
+    newSpan2.append('(')
+    newSpan2.append(document.createTextNode(city.value))
+    newSpan2.append(')')
+
     newDiv2.appendChild(newDiv4)
-    
+
     document.getElementById('posts').appendChild(newDiv)
+    
+}
 
+function textFilter(){
+    //var textArray = []
+    /*
+    for(var i =0; i<des.value.length; i++){
+        textArray[i] = des.value.split('')
+    }
+    */
+   console.log(filterTxt.value)
 
+   var textArray = filterTxt.value.split('')
+   for(var i = 0; i< textArray.length; i++){
+       console.log(textArray[i])
+   }
+
+   var titleValue= title.value.toLocaleLowerCase()
+
+   for(var i = 0; i< post.length; i++)
+   if(titleValue.includes(filterTxt.value)){
+        post[i].style.display= "flex"
+   }
+   else{
+       post[i].style.display = 'none'
+   }
+       
+   
 }
 
 createPost.onclick = function(event){
-    console.log('click')
-    registerPost()
-    console.log('complete')
-    closeModal()
+
+    if(!des.value || !price.value || !city.value){
+        alert("invalid input!")
+   }
+   else{
+       registerPost()
+       closeModal()
+   }
 }
 
+updateButton.onclick = function(event){
+    console.log('click')
+    textFilter()
+}
